476fc0140767d968c929b0c8bb58cd7357ce02d4
LegoBricker <dylan@mygreenfamily.us>
2019-12-29 21:51:14 -0800

    Add ra/dec -> alt/az coordinate conversion.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ var svg = d3.select("body").append("svg").attr("width", 1024).attr("height", 102
 // // Image object. At least the syntax reminds me heavily of Nim.
 var img = svg.append("svg:image").attr("xlink:href", src).attr("width", 1024).attr("height", 1024);
 
+// Roughly but not exactly the RA/Dec of Polaris.
+var ra = 2.5 * 15;
+var dec = 89;
+
 // Start of the J2000 Epoch is January 1, 2000 at 1200 UT.
 // So year = 2000, month = 0 (January), day = 1, hour = 12
 // minute = 0, second = 0
@@ -24,14 +28,15 @@ let days = seconds / 3600 / 24; // 3600 seconds in an hour 24 hours per day
 
 // Finds the number of hours into the universal time day we are
 let today = new Date();
+// + 8 is due to Irvine
+// TODO: Change to + 7 when deployed in Arizona
 var ut = today.getHours() + 8 + today.getMinutes() / 60; // + 8 to get from local to UT
 
 // Makes sure universal time is on 24 hour time and not 48.
 ut = (ut < 24) ? ut : ut - 24;
 
 // Calculates Local Sidereal Time
-// Currently set to sidreal time in Irvine, where I'm coding this.
-var lst = 100.46 + 0.985647 * days + (-117.826508) + 15 * ut;
+var lst = 100.46 + 0.985647 * days + pos[1] + 15 * ut;
 
 // First step to bringing LST to within 0-360.
 // Days is usually close to 7200 so this is a good first approximation
@@ -42,13 +47,35 @@ lst = (lst < 0) ? lst + 360 : lst;
 lst = (lst > 360) ? lst - 360 : lst;
 // I double checked this as well and it is within a minute of real LST
 
+var hour = lst - ra; // Hour angle
+
+// Conversions to radians
+dec = dec * Math.PI / 180;
+ra = ra * Math.PI / 180;
+hour = hour * Math.PI / 180;
+var lat = pos[0] * Math.PI / 180;
+
+// Sine of altitude hence taking asin after.
+var alt = Math.sin(dec) * Math.sin(lat) + Math.cos(dec) * Math.cos(lat) * Math.cos(hour);
+alt = Math.asin(alt)
+
+var az = (Math.sin(dec) - Math.sin(alt) * Math.sin(lat)) / (Math.cos(alt) * Math.cos(lat));
+
+// Conversion to degrees, which is needed for the interpolation.
+az = Math.acos(az) * 180 / Math.PI;
+alt = alt * 180 / Math.PI;
+
+// Makes sure the azimuthal angle is in a good range.
+if (Math.sin(hour) > 0) {
+  az = 360 - az;
+}
 
 // Interpolation tables.
 const r_sw = [0, 55, 110, 165, 220, 275, 330, 385, 435, 480, 510];
 const theta_sw = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95];
 
-var alt = 20;
-var az = 90;
+// var alt = 20;
+// var az = 90;
 
 alt = 90 - alt; // Since alt is measured up from the horizon and we need it from center
 let ind = theta_sw.indexOf(alt); // Checks to see if the item is in the conversion already
@@ -74,14 +101,9 @@ else {
   }
 }
 
-let x = 512 - r * Math.sin(az * Math.PI / 180)
-let y = 512 - r * Math.cos(az * Math.PI / 180)
+let x = 512 - r * Math.sin(az * Math.PI / 180);
+let y = 512 - r * Math.cos(az * Math.PI / 180);
 
 // Circle at the given position.
 // I didn't know that chartreuse was a green color.
 svg.append("circle").attr("r", 3.5).attr("cx", x).attr("cy", y).attr("style", "fill: chartreuse");
-
-console.log("testing here")
-console.log(r)
-console.log(x)
-console.log(y)
