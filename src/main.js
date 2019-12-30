b06c05ca02436fa0c38a41ec79e92cfa017cc1f1
LegoBricker <dylan@mygreenfamily.us>
2019-12-29 16:19:52 -0800

    Add a circle at the zenith.
    - This is the one I'll use for the telescope pointing... eventually.

--- src/main.js
+++ src/main.js
@@ -6,3 +6,6 @@ var svg = d3.select("body").append("svg").attr("width", 1024).attr("height", 102
 
 // // Image object. At least the syntax reminds me heavily of Nim.
 var img = svg.append("svg:image").attr("xlink:href", src).attr("width", 1024).attr("height", 1024);
+
+// Circle at the zenith position.
+svg.append("circle").attr("r", 3.5).attr("cx", 512).attr("cy", 512).attr("style", "fill: chartreuse")
